e1593996f35958e50364a79dadf42d1242d73fed
seelneas <seelneas@users.noreply.github.com>
2026-03-15 19:53:17 +0000

    deploy: f9bffa21d79caf4be24117c0986f9762dda16369

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2000261828"
+    serviceWorkerVersion: "3502198499"
   }
 });
